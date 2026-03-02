931ee86fe95a27fcbe48900e73e545ad529d7f3e
SoClear <48470201+SoClear@users.noreply.github.com>
2026-03-02 20:55:03 +0800

    build: bump version to 1.1.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "io.github.soclear.edgex"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
