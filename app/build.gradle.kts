99b2e8508e5948ffc2c69b89e961a908a0dbd27e
SoClear <48470201+SoClear@users.noreply.github.com>
2026-06-07 00:18:10 +0800

    build: bump version to 2.2.0 (12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.github.soclear.edgex"
         minSdk = 30
         targetSdk = 37
-        versionCode = 11
-        versionName = "2.1.1"
+        versionCode = 12
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
